3114d7bded07fa04bafa507699f19d6548f7100d
Aharon Fajardo <Aharon.Fajardo@arrowdigital.com>
2019-07-02 17:26:10 -0400

    updated html and css to abide by BEM CSS conventions

--- gulp/images.js
+++ gulp/images.js
@@ -1,8 +1,13 @@
 const gulp = require("gulp");
-const $ = require("gulp-load-plugins")({ lazy: true });
+const $ = require("gulp-load-plugins")({
+  lazy: true,
+});
 const resizer = require("@zellwk/resize-images");
 
-const { images, tmpPath } = require("./_config");
+const {
+  images,
+  tmpPath,
+} = require("./_config");
 
 const src = images.source;
 const build = images.build;
@@ -14,7 +19,12 @@ const tmpOutput2 = `${tmpPath}/resized`;
 function jpegToWebp() {
   return gulp
     .src(`${src}.{jpeg,jpg}`)
-    .pipe($.newer({ dest: tmpOutput, ext: ".webp" }))
+    .pipe(
+      $.newer({
+        dest: tmpOutput,
+        ext: ".webp",
+      }),
+    )
     .pipe($.webp({ quality: 80 }))
     .pipe(gulp.dest(tmpOutput));
 }
@@ -22,7 +32,12 @@ function jpegToWebp() {
 function pngToWebp() {
   return gulp
     .src(`${src}.png`)
-    .pipe($.newer({ dest: tmpOutput, ext: ".webp" }))
+    .pipe(
+      $.newer({
+        dest: tmpOutput,
+        ext: ".webp",
+      }),
+    )
     .pipe($.webp({ lossless: true }))
     .pipe(gulp.dest(tmpOutput));
 }
@@ -31,7 +46,16 @@ function minifyImages() {
   return gulp
     .src(`${src}.{png,jpg,jpeg,gif}`)
     .pipe($.newer(tmpOutput))
-    .pipe($.imagemin([$.imagemin.jpegtran({ progressive: true }), $.imagemin.optipng({ optimizationLevel: 5 })]))
+    .pipe(
+      $.imagemin([
+        $.imagemin.jpegtran({
+          progressive: true,
+        }),
+        $.imagemin.optipng({
+          optimizationLevel: 5,
+        }),
+      ]),
+    )
     .pipe(gulp.dest(tmpOutput));
 }
 
@@ -44,17 +68,28 @@ function resizeImages() {
 }
 
 function copyImagesToDist() {
-  return gulp.src(`${tmpOutput2}/**/*`).pipe(gulp.dest(build));
+  return gulp
+    .src(`${tmpOutput2}/**/*`)
+    .pipe(gulp.dest(build));
 }
 
 function copySvgToDist() {
-  return gulp.src(`${src}/**/*.svg`).pipe(gulp.dest(build));
+  return gulp
+    .src(`${src}/**/*.svg`)
+    .pipe(gulp.dest(build));
 }
 
 const compileImages = gulp.series(
-  gulp.parallel(jpegToWebp, pngToWebp, minifyImages),
+  gulp.parallel(
+    jpegToWebp,
+    pngToWebp,
+    minifyImages,
+  ),
   gulp.parallel(resizeImages),
-  gulp.parallel(copyImagesToDist, copySvgToDist),
+  gulp.parallel(
+    copyImagesToDist,
+    copySvgToDist,
+  ),
 );
 
 exports.src = src;
